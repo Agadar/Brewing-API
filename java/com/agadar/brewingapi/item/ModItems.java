fbc56c6e458fe38d4353aad215f6950af766de77
Martin <Martin@Martin-PC>
2014-07-26 04:29:42 +0200

    Fixed some stuff so that it actually works.

--- java/com/agadar/brewingapi/item/ModItems.java
+++ java/com/agadar/brewingapi/item/ModItems.java
@@ -1,5 +1,7 @@
 package com.agadar.brewingapi.item;
 
+import java.util.ArrayList;
+
 import com.agadar.brewingapi.help.RegisterHelper;
 
 import cpw.mods.fml.common.registry.GameRegistry;
@@ -7,6 +9,8 @@ import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
 
 /** Manages all mod items. */
 public class ModItems 
@@ -16,7 +20,20 @@ public class ModItems
 	/** Instantiates and registers all mod items. */
 	public static void registerModItems()
 	{
-		RegisterHelper.registerItem(brewing_stand2);		
+		RegisterHelper.registerItem(brewing_stand2);	
+		removeRecipe(new ItemStack(Items.brewing_stand));
 		GameRegistry.addRecipe(new ItemStack(brewing_stand2), " x ", "yyy", 'x', Items.blaze_rod, 'y', Blocks.cobblestone);
+	}
+	
+	/** Removes all recipes from the CraftingManager's recipe list that have an output equal to par1ItemStack. */
+	private static void removeRecipe(ItemStack par1ItemStack) 
+	{
+		ArrayList<?> recipes = (ArrayList<?>) CraftingManager.getInstance().getRecipeList();
+
+		for (int i = 0; i < recipes.size(); i++) 
+		{
+			IRecipe tmpRecipe = (IRecipe) recipes.get(i);
+			if (ItemStack.areItemStacksEqual(par1ItemStack, tmpRecipe.getRecipeOutput())) recipes.remove(i);
+		}
 	}
 }
